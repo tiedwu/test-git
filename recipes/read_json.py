da8ac7a9c106b9e1be3fcb64f44c57dea31ec19a
tiedwu <tiedwu@gmail.com>
2022-05-13 17:45:46 +0800

    remove recipes

--- recipes/read_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-file = "camp_history.json"
-with open(file, "r") as f:
-	dict = json.load(f)
-	print(dict)
-	view_data = [{'index': k, 'content': dict[k]} for k in dict.keys()]
-	print(view_data)
